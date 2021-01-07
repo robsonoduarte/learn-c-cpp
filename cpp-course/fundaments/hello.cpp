c2b6072c06c4416e1537c672033bb4abab71921a
Robson Duarte <robson.o.d@gmail.com>
2021-01-06 21:16:39 -0300

    :rocket:

---
+++ cpp-course/fundaments/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+
+    // using scope resolution operator with namespace C++ to print on console
+    std::cout << "Hello C++" << std::endl;
+
+    // using C functions to print on console
+    printf("Hello C++ \n");
+    puts("Hello C++");
+
+    return 0;
+}
