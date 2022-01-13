13481376ca9a2a5d84c768202926f28af8ba52f4
Robson Duarte <robson.o.d@gmail.com>
2022-01-12 21:55:30 -0300

    io example

---
+++ cpp-course/fundaments/io.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name;
+
+    // insertion operator
+    cout << "Enter your name:" << endl;
+
+    // extraction operator
+    cin >> name;
+
+    cout << "Your name is: " <<  name << endl;
+
+    return 0;
+}
