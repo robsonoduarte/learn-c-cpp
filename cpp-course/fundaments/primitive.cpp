ec6f721e902e79c9f3377c328467d5f94d571436
Robson Duarte <robson.o.d@gmail.com>
2022-01-13 22:22:12 -0300

    primitives types

---
+++ cpp-course/fundaments/primitive.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    bool isAdmin = 10; // the value should be true, false, 0 (same false) and any number that will be true
+    cout << isAdmin << endl;
+}
