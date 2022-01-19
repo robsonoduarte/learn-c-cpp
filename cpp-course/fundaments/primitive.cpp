0fea7802e8e0c5ce1f54f49270f14fff82b192d2
Robson Duarte <robson.o.d@gmail.com>
2022-01-18 22:01:38 -0300

    :pray:

--- cpp-course/fundaments/primitive.cpp
+++ cpp-course/fundaments/primitive.cpp
@@ -5,4 +5,18 @@ using namespace std;
 int main() {
     bool isAdmin = 10; // the value should be true, false, 0 (same false) and any number that will be true
     cout << isAdmin << endl;
+
+    //char symbol = '#';
+    char symbol('#') ; // other valid syntax to init a var
+    cout << symbol << endl;
+
+    // datatype modifiers
+    // singed unsigned long short
+    unsigned short int age = 45;
+    cout << age << endl;
+
+    float pi = 3.14;
+    const double PI = 3.14;
+    cout << pi << endl;
+    cout << PI << endl;
 }
