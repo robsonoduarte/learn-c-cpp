7a987b2162413aff4e05e675a7b6a76802ee87ab
Robson Duarte <robson.o.d@gmail.com>
2022-05-25 23:06:45 -0300

    :rocket:

--- cpp-course/fundaments/while.cpp
+++ cpp-course/fundaments/while.cpp
@@ -18,5 +18,19 @@ int main() {
 
     cout << (double) total / qty << endl;
 
+    int i = 0;
+
+    while (i < 10) {
+        cout << i << endl;
+        i++;
+    }
+
+    int j = 100;
+
+    while (j >= 0) {
+        cout << j << endl;
+        j -= 2;
+    }
+
     return 0;
 }
