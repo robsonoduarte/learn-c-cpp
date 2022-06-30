5dbccc2398cbd9bfcda58d1bb1db307c81f52cf5
Robson Duarte <robson.o.d@gmail.com>
2022-06-29 22:05:16 -0300

    more class (#35)
    Co-authored-by: Robson Duarte <robosn.o.d@gmail.com>

---
+++ cpp-course/functions/typedef.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+typedef int id;
+using counter = long;
+
+int main() {
+    id a = 5;
+    counter b = 10;
+    cout << a + b << endl;
+    return 0;
+}
