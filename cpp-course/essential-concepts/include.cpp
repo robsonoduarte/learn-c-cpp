10322aee4ca0b4b08276b5197e76c1b793ff045f
Robson Duarte <robosn.o.d@gmail.com>
2022-07-12 22:28:07 -0300

    essentail-concepts class 70 to 75

---
+++ cpp-course/essential-concepts/include.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "includes/include.h"
+using namespace std;
+
+int main(){
+    cout << add(1,7) << endl;
+    return 0;
+}
+
+int add(int a, int b){
+    return a + b;
+}
